920399cec7af77319eeac6f67fa02a7b6e248256
68686 <010101@111.com>
2023-03-15 20:37:29 +0800

    111

--- source/js/txmap.js
+++ source/js/txmap.js
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(116.28676, 40.05198, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(116.280721, 40.04583, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
